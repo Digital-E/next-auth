4251209978c569260ffd134de764952e28eaed3e
SamuelBassett <samabassett@gmail.com>
2022-11-08 12:18:37 +0100

    first commit

---
+++ providers/apple.js
@@ -0,0 +1,52 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = Apple;
+
+function Apple(options) {
+  return {
+    id: "apple",
+    name: "Apple",
+    type: "oauth",
+    wellKnown: "https://appleid.apple.com/.well-known/openid-configuration",
+    authorization: {
+      params: {
+        scope: "name email",
+        response_mode: "form_post"
+      }
+    },
+    userinfo: {
+      request({ provider, tokens, requestInfo: { body } }) {
+        const profile = tokens.claims()
+        if (body?.user) {
+        try {
+          profile.user = typeof body.user === 'string' ? JSON.parse(body.user) : body.user
+
+          return profile
+        } catch (error) {
+          profile.user = body.user
+          // logger.debug("ERR_PARSING_BODY_USER_OBJECT_APPLE", {
+          //   error: error as Error,
+          //   providerId: provider.id,
+          // })
+        } 
+      }
+      },
+    },    
+    idToken: true,
+
+    profile(profile) {
+      return {
+        id: profile.sub,
+        name: profile.user?.name,
+        email: profile.email,
+        image: null
+      };
+    },
+
+    checks: ["pkce"],
+    options
+  };
+}
